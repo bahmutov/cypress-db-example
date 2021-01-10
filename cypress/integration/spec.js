6b10a31b443926be0300e8df62b7810344aa01e1
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-01-10 15:42:58 -0500

    tests fail without database reset

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -4,4 +4,20 @@ describe('Movie database', () => {
   it('starts with an empty list', () => {
     cy.request('/persons').its('body').should('deep.equal', [])
   })
+
+  it('adds an actor without any movies', () => {
+    cy.request('POST', '/persons', {
+      firstName: 'Joe',
+      lastName: 'Smith',
+    })
+    // now there should be 1 actor
+    cy.request('/persons')
+      .its('body')
+      .should('have.length', 1)
+      .its('0')
+      .should('include', {
+        firstName: 'Joe',
+        lastName: 'Smith',
+      })
+  })
 })
