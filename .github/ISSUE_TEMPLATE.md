c1eefb6eeec727736eb831b32bfd7738da06817b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-01-10 15:14:08 -0500

    remove some files

--- .github/ISSUE_TEMPLATE.md
+++
@@ -1,3 +0,0 @@
-Please read the instructions linked below before opening an issue.
-
-https://vincit.github.io/objection.js/guide/contributing.html#issues
--- publish-docs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-npm run docs:build
-rm -rf ../objection-doc/*
-mv doc/.vuepress/dist/* ../objection-doc/
-cd ../objection-doc
-git add -A
-git commit -m "update docs"
-git push origin gh-pages:gh-pages
