5e7bd7ac44aa3370530075993c0cb9a96c2bfa0a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-01-11 12:42:40 -0500

    add comments

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Cypress DB example [![ci status][ci image]][ci url]
 
+The application in [app.js](app.js) and [api.js](api.js) uses [Objection.js](https://github.com/Vincit/objection.js) to provide REST API on top of Sqlite3 database.
+
+The Cypress spec files in [cypress/integration](cypress/integration) test the app by executing `cy.request` calls. To reset the database data and query the tables directly, we can use `cy.task` commands inside [cypress/plugins/index.js](cypress/plugins/index.js) file.
+
 [ci image]: https://github.com/bahmutov/cypress-db-example/workflows/ci/badge.svg?branch=main
 [ci url]: https://github.com/bahmutov/cypress-db-example/actions
+
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,9 +1,17 @@
+// let's assert parameters in our task calls, for this
+// we have installed Chai NPM module and use it in the way
+// similar to the specs
 const { expect } = require('chai')
+
+// we can connect to the database using the same logic as the app does
 const Knex = require('knex')
 const knexConfig = require('../../knexfile')
 const { Model } = require('objection')
 const knex = Knex(knexConfig.development)
 Model.knex(knex)
+
+// if we want to query the Person table, we can use
+// the same model class as the app does
 const Person = require('../../models/Person')
 
 /**
@@ -11,11 +19,19 @@ const Person = require('../../models/Person')
  */
 module.exports = (on, config) => {
   on('task', {
+    // before every test we should reset the data to a known good state
     resetPeopleTable() {
       console.log('reset People table')
+      // this call returns a Promise, working nicely
+      // with cy.task (which expects a value or a Promise)
       return Person.query().truncate()
     },
+
+    // we can use the "async/await" syntax, since Cypress
+    // comes with the bundled Node v12
     async findPerson(id) {
+      // an example of using Chai assertions to validate
+      // data inside the plugins file
       expect(id, 'valid id').to.be.a('number').above(0)
       console.log('looking for person with id %d', id)
       const p = await Person.query().findById(id)
